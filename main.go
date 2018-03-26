7914a652fbcc673616aecb508732bff3f8949774
Malcolm Foster <malcolmfoster@Malcolms-MacBook-Air.local>
2018-03-26 10:48:56 -0600

    port

--- main.go
+++ main.go
@@ -4,12 +4,18 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/handlers"
 	"github.com/mfoster1989/WishBook_Server/messages"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
 	router := messages.NewRouter() // create routes
 	// these two lines are important in order to allow access from the front-end side to the methods
 	allowedOrigins := handlers.AllowedOrigins([]string{"*"})
