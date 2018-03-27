72c78480c456b14b758601bfcf1fccc13ecd347f
Malcolm Foster <malcolmfoster@gmail.com>
2018-03-27 14:51:24 -0600

    returns a slice

--- messages/respository.go
+++ messages/respository.go
@@ -21,14 +21,14 @@ const DBNAME = "heroku_qcv9wrjh"
 const DOCNAME = "messages"
 
 // GetMessages returns the list of Messages
-func (r Repository) GetMessages() Messages {
+func (r Repository) GetMessages() []Messages {
 	session, err := mgo.Dial(SERVER)
 	if err != nil {
 		fmt.Println("Failed to establish connection to Mongo server:", err)
 	}
 	defer session.Close()
 	c := session.DB(DBNAME).C(DOCNAME)
-	results := Messages{}
+	var results []Messages
 	if err := c.Find(nil).All(&results); err != nil {
 		fmt.Println("Failed to write results:", err)
 	}
