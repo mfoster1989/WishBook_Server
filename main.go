eb20d15ce2dc94a124a9c487ea0cf2885929c505
Malcolm Foster <malcolmfoster@gmail.com>
2018-03-27 10:40:18 -0600

    fixed list and serve

--- main.go
+++ main.go
@@ -21,6 +21,6 @@ func main() {
 	allowedOrigins := handlers.AllowedOrigins([]string{"*"})
 	allowedMethods := handlers.AllowedMethods([]string{"GET", "POST", "DELETE", "PUT"})
 	// launch server with CORS validations
-	log.Fatal(http.ListenAndServe("process.env.PORT || :9000",
+	log.Fatal(http.ListenAndServe(":"+port,
 		handlers.CORS(allowedOrigins, allowedMethods)(router)))
 }
